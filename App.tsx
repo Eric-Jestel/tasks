203afc528cac5c7d3c1f17da3660b8c265b662d6
eric-jestel <eric.jestel@maine.edu>
2024-09-10 22:14:16 -0400

    Added Hello World to App

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App(): React.JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">
+                UM COS420 with React Hooks and TypeScript
+            </header>
+            <p>
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload. Eric Jestel
+            </p>
+            <p>Hello World</p>
+        </div>
+    );
+}
+
+export default App;

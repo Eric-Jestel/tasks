f17f575f0813d440791ab60b638ecedb728b61c1
eric-jestel <eric.jestel@maine.edu>
2024-11-01 20:36:58 -0400

    HW 11-1 interactives

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -7,10 +7,38 @@ import { Button } from "react-bootstrap";
  * module (which returns a random decimal between 0 up until 1) in order
  * to produce a random integer between 1 and 6 (inclusive).
  */
+
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(1);
+    const [rightDie, setRightDie] = useState<number>(2);
+
+    const rollLeftDie = () => {
+        setLeftDie(d6());
+    };
+    const rollRightDie = () => {
+        setRightDie(d6());
+    };
+
+    const message =
+        leftDie === 1 && rightDie === 1 ? "Lose"
+        : leftDie === rightDie ? "Win"
+        : "";
+
+    return (
+        <div>
+            <div>
+                <span data-testid="left-die">{leftDie}</span>
+                <Button onClick={rollLeftDie}>Roll Left</Button>
+            </div>
+            <div>
+                <span data-testid="right-die">{rightDie}</span>
+                <Button onClick={rollRightDie}>Roll Right</Button>
+            </div>
+            <div>{message || ""}</div>
+        </div>
+    );
 }
